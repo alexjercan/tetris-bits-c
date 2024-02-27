a874c393a6867d9d632eaf3a13770ab443704e53
Alex Jercan <jercan_alex27@yahoo.com>
2024-02-27 19:31:21 +0200

    refactor: change to use stdint

--- src/tetris/tetris.c
+++ src/tetris/tetris.c
@@ -1,14 +1,20 @@
 #include "tetris.h"
 
-const int pieces[] = {
-    0 | (0 << 8) | (0b00011000 << 16) | (0b00011000 << 24), // O
-    0b00001000 | (0b00001000 << 8) | (0b00001000 << 16) |
-        (0b00001000 << 24),                                          // I
-    0 | (0 << 8) | (0b00011000 << 16) | (0b00001100 << 24),          // S
-    0 | (0 << 8) | (0b00011000 << 16) | (0b00110000 << 24),          // Z
-    0 | (0b00010000 << 8) | (0b00010000 << 16) | (0b00011000 << 24), // L
-    0 | (0b00001000 << 8) | (0b00001000 << 16) | (0b00011000 << 24), // J
-    0 | (0 << 8) | (0b00011100 << 16) | (0b00001000 << 24),          // T
+const uint32_t pieces[] = {
+    0 | (0 << 8) | ((uint32_t)0b00011000 << 16) |
+        ((uint32_t)0b00011000 << 24), // O
+    0b00001000 | ((uint32_t)0b00001000 << 8) | ((uint32_t)0b00001000 << 16) |
+        ((uint32_t)0b00001000 << 24), // I
+    0 | (0 << 8) | ((uint32_t)0b00011000 << 16) |
+        ((uint32_t)0b00001100 << 24), // S
+    0 | (0 << 8) | ((uint32_t)0b00011000 << 16) |
+        ((uint32_t)0b00110000 << 24), // Z
+    0 | ((uint32_t)0b00010000 << 8) | ((uint32_t)0b00010000 << 16) |
+        ((uint32_t)0b00011000 << 24), // L
+    0 | ((uint32_t)0b00001000 << 8) | ((uint32_t)0b00001000 << 16) |
+        ((uint32_t)0b00011000 << 24), // J
+    0 | (0 << 8) | ((uint32_t)0b00011100 << 16) |
+        ((uint32_t)0b00001000 << 24), // T
 };
 #define PIECE_COUNT (sizeof(pieces) / sizeof(pieces[0]))
 
@@ -22,13 +28,13 @@ int tetris_init(struct tetris *t) {
 }
 
 int tetris_spawn(struct tetris *t) {
-    int piece = pieces[tetris_rand_int(PIECE_COUNT)];
+    uint32_t piece = pieces[tetris_rand_int(PIECE_COUNT)];
 
     int is_occupied = (t->board[0] & piece) | (t->board[1] & (piece >> 8)) |
                       (t->board[2] & (piece >> 16)) |
                       (t->board[3] & (piece >> 24));
 
-    for (int i = 0; i < BOARD_BUFFER; i++) {
+    for (unsigned int i = 0; i < BOARD_BUFFER; i++) {
         t->board[i] = (piece >> (i * 8)) & 0b11111111;
     }
 
@@ -39,7 +45,7 @@ int tetris_tick(struct tetris *t, enum move m) {
     int can_move = 1;
 
     for (int i = BOARD_HEIGHT + BOARD_BUFFER - 2; i >= 0; i--) {
-        unsigned char free = (t->board[i] ^ t->board[i + 1]) & t->board[i];
+        uint8_t free = (t->board[i] ^ t->board[i + 1]) & t->board[i];
 
         t->board[i] = t->board[i] & (~free);
 
@@ -47,7 +53,7 @@ int tetris_tick(struct tetris *t, enum move m) {
         switch (m) {
         case MOVE_LEFT: {
             if (can_move) {
-                unsigned char mask = 0;
+                uint8_t mask = 0;
                 if (tmp & (1 << (BOARD_WIDTH - 1))) {
                     mask = 1;
                 }
@@ -57,7 +63,7 @@ int tetris_tick(struct tetris *t, enum move m) {
         }
         case MOVE_RIGHT: {
             if (can_move) {
-                unsigned char mask = 0;
+                uint8_t mask = 0;
                 if (tmp & 1) {
                     mask = 1 << (BOARD_WIDTH - 1);
                 }
@@ -82,11 +88,11 @@ int tetris_tick(struct tetris *t, enum move m) {
         t->board[i + 1] = t->board[i + 1] | free;
     }
 
-    unsigned char mask = 0b11111111;
-    int count = 0;
+    uint8_t mask = 0b11111111;
+    uint32_t count = 0;
     while (t->board[BOARD_HEIGHT + BOARD_BUFFER - 1] == mask) {
         count++;
-        for (int i = BOARD_HEIGHT + BOARD_BUFFER - 1; i > BOARD_BUFFER; i--) {
+        for (unsigned int i = BOARD_HEIGHT + BOARD_BUFFER - 1; i > BOARD_BUFFER; i--) {
             t->board[i] = t->board[i - 1];
         }
     }
--- src/tetris/tetris.h
+++ src/tetris/tetris.h
@@ -1,7 +1,9 @@
 #ifndef TETRIS_H
 #define TETRIS_H
 
-#define BOARD_WIDTH sizeof(unsigned char) * 8
+#include <stdint.h>
+
+#define BOARD_WIDTH sizeof(uint8_t) * 8
 #define BOARD_HEIGHT 12
 #define BOARD_BUFFER 4
 
@@ -12,8 +14,8 @@ enum move {
 };
 
 struct tetris {
-        int score;
-        unsigned char board[BOARD_HEIGHT + BOARD_BUFFER];
+        uint32_t score;
+        uint8_t board[BOARD_HEIGHT + BOARD_BUFFER];
 };
 
 int tetris_rand_int(int max);
